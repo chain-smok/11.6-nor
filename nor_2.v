4efc54468dbee6759ed33bc83047ef94d7413567
chain-smok <s1101780@gm.pu.edu.tw>
2022-11-06 16:54:34 +0800

    update nor

--- nor_2.v
+++ nor_2.v
@@ -5,18 +5,16 @@ module nor_2(NOR_OUT,A,B);
     supply1 PWR;
     wire w1,N_A,N_B;
     pmos (N_A,PWR,A);
-	 nmos (N_A,GND,A);
+	nmos (N_A,GND,A);
 
-	 pmos (N_B,PWR,B);
-	 nmos (N_B,GND,B);
-
-    nmos (w1,GND,N_A);
-    nmos (NOR_OUT,w1,N_B);
+	pmos (N_B,PWR,B);
+	nmos (N_B,GND,B);
 
     pmos (NOR_OUT,PWR,N_A);
     pmos (NOR_OUT,PWR,N_B);
     
-    
+    nmos (NOR_OUT,w1,N_A);   
+    nmos (w1,GND,N_B); 
 
 endmodule
     
