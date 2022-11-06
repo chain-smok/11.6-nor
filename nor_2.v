cbae265e5fb3f4384ea2a4c2cf31f4d9199495f2
chain-smok <s1101780@gm.pu.edu.tw>
2022-11-06 20:13:09 +0800

    remove not

--- nor_2.v
+++ nor_2.v
@@ -10,11 +10,11 @@ module nor_2(NOR_OUT,A,B);
 	pmos (N_B,PWR,B);
 	nmos (N_B,GND,B);
 
-    pmos (w1,PWR,N_A);   
-    pmos (NOR_OUT,w1,N_B); 
+    pmos (w1,PWR,A);   
+    pmos (NOR_OUT,w1,B); 
 
-    nmos (NOR_OUT,GND,N_A);
-    nmos (NOR_OUT,GND,N_B);
+    nmos (NOR_OUT,GND,A);
+    nmos (NOR_OUT,GND,B);
     
 endmodule
     
